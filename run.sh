22aab62f3697ca921e7bfbd570a61beae7caf792
mijdavis2 <mdavisinsc@gmail.com>
2016-12-20 13:17:30 -0800

    Add common node modules

--- run.sh
+++ run.sh
@@ -2,17 +2,20 @@
 
 # Add repositories and install Essentials
 sudo add-apt-repository -y ppa:webupd8team/java
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get upgrade -yq
-sudo apt-get install -y build-essentials git vim curl
+sudo apt-get install -y build-essentials git vim curl yarn
 
 # Install Java8
 sudo apt-get install -y python-software-properties debconf-utils
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 
-# Install Nodejs
+# Install Nodejs and globals
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install -g gulp firebase-tools aureali-cli
 
 # Install Python3
 sudo apt-get install -y python3
