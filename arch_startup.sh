b82ccaa0cb05effd9c3027cff683404b62d44df0
MJ Davis <mijdavis2@users.noreply.github.com>
2018-01-27 07:24:08 -0800

    Add ethereum development packages

--- arch_startup.sh
+++ arch_startup.sh
@@ -12,19 +12,25 @@ sudo pacman -Syu
 #########
 sudo pacman -S vim
 
-# Awesome-Vim Plugins #
-# Note: makes pathogen (vim package manager) directory use ~/.vim_runtime/sources_non_forked
+# Awesome-Vim package bundle
+# Note: makes pathogen (vim package manager) directory use ~/.vim_runtime/sources_non_forked instead of ~/.vim/bundle
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-# Ethereum vim plugin
-git clone https://github.com/tomlion/vim-solidity.git ~/.vim_runtime/sources_non_forked/vim-solidity
 
 #########################################
 ## NVM (node version manager) and Node ##
 #########################################
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
+
+###################################
+## Ethereum development packages ##
+###################################
+# Vim plugin
+git clone https://github.com/tomlion/vim-solidity.git ~/.vim_runtime/sources_non_forked/vim-solidity
+
+# Ethereum in-mem blockchain and solidity compiler
+npm install -g ganache-cli
+npm install -g solc
