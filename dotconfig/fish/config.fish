5646a2ddf8dc474fb68d2bc6defb41c985833fa6
mijdavis2 <mdavisinsc@gmail.com>
2020-10-04 12:19:31 -0600

    Remove fundle

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -15,11 +15,6 @@ set -x PATH "$HOME/.pyenv/bin" $PATH
 status --is-interactive; and . (pyenv init -|psub)
 #status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
-# Fundle plugin management
-fundle plugin 'edc/bass'
-
-fundle init
-
 # Path configs
 set -x N_PREFIX $HOME
 set -x PATH $HOME/n/bin $PATH
